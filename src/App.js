b53ccd4c86ab60732b887cf79172239c7c6756da
Víctor Velarde <victor.velarde@gmail.com>
2020-09-29 16:23:54 +0200

    feat: add basemap selector to the main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Map } from './features/map/Map';
 import './App.css';
+import { BasemapSelector } from './features/basemapSelector/BasemapSelector';
 
 function App() {
   return (
     <div className="App">
       <Map />
+      <BasemapSelector/>
     </div>
   );
 }
