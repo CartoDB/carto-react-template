f59ee0ae7eb1fe9a8533f8e00af3976af0273d05
Alberto Arana <aarana@carto.com>
2020-10-14 17:03:18 +0200

    Removed unnecessary style

--- src/components/views/Components/Components.js
+++ src/components/views/Components/Components.js
@@ -5,11 +5,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
 }));
 
 function Components() {
