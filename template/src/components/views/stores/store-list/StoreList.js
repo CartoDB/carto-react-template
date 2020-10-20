7281bd5d2e440e93c6b4abb3b1a926ee10ccc5fa
Raúl Yeguas <neokore@gmail.com>
2020-10-20 12:16:57 +0200

    Remove unused import

--- template/src/components/views/stores/store-list/StoreList.js
+++ template/src/components/views/stores/store-list/StoreList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Divider, Typography } from '@material-ui/core';
 import { FormulaWidget } from 'components/common/widgets/FormulaWidget';
