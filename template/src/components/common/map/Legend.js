375a875373da45ea0dc059d671fe76854b8ae261
Raúl Yeguas <neokore@gmail.com>
2020-10-22 08:01:47 +0200

    KPIs (raw feature) [ch110708]

--- template/src/components/common/map/Legend.js
+++ template/src/components/common/map/Legend.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { LayerStyle as StoreLayerStyle } from './layers/StoresLayer';
+import { LayerStyle as RevenueByStateLayerStyle } from './layers/RevenueByStateLayer';
 import { CategoryLegendUI } from 'lib/react-ui/index';
 
 export function Legend(props) {
   const styles = {};
   styles[StoreLayerStyle.id] = StoreLayerStyle;
+  styles[RevenueByStateLayerStyle.id] = RevenueByStateLayerStyle;
 
   const layers = useSelector((state) => Object.keys(state.carto.layers));
 
   return (
     <div style={props.style}>
-      {layers.map((layerId) => (
-        <CategoryLegendUI key={layerId} categories={styles[layerId]} />
-      ))}
+      {layers.map(
+        (layerId) =>
+          styles[layerId] && (
+            <CategoryLegendUI key={layerId} categories={styles[layerId]} />
+          )
+      )}
     </div>
   );
 }
