7f282b526e8d532f155b32f4a2b5a6ff4ca627b4
builtbyedgar <eledgaar@gmail.com>
2020-10-22 09:39:07 +0200

    wrapper widget ui options and actions

--- template/src/lib/react-ui/widgets/WrapperWidgetUI.js
+++ template/src/lib/react-ui/widgets/WrapperWidgetUI.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Button, Collapse, Grid, Icon } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Collapse,
+  Grid,
+  Icon,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     width: '100%',
     height: '56px',
-    paddingRight: theme.spacing(3),
+    paddingRight: theme.spacing(2),
     paddingLeft: theme.spacing(2),
   },
   button: {
@@ -52,15 +62,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const ICONS = ['icon-content-minimize.svg', 'icon-content-maximize.svg'];
+
 function WrapperWidgetUI(props) {
+  const wrapper = createRef();
   const classes = useStyles();
   const [expanded, setExpanded] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-  const icons = ['icon-content-minimize.svg', 'icon-content-maximize.svg'];
   const handleExpandClick = () => {
     if (props.expandable) {
       setExpanded(!expanded);
     }
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOptionAction = (action) => {
+    if (action) {
+      action();
+    }
+
+    handleClose();
   };
 
   return (
@@ -71,7 +101,7 @@ function WrapperWidgetUI(props) {
           startIcon={
             props.expandable && (
               <Icon className={classes.icon}>
-                <img src={expanded ? icons[0] : icons[1]} alt='Icon' />
+                <img src={expanded ? ICONS[0] : ICONS[1]} alt='Icon' />
               </Icon>
             )
           }
@@ -80,9 +110,66 @@ function WrapperWidgetUI(props) {
           {props.title}
         </Button>
 
-        <Grid item>actions here</Grid>
+        <Grid item>
+          {props.actions.map((action) => {
+            return (
+              <IconButton
+                key={action.id}
+                aria-label={action.label}
+                onClick={action.action}
+              >
+                <img src={action.icon} alt={action.name} />
+              </IconButton>
+            );
+          })}
+
+          {
+            <IconButton
+              aria-label='options'
+              aria-controls='long-menu'
+              aria-haspopup='true'
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+          }
+          <Menu
+            id='options-menu'
+            elevation={3}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            anchorEl={anchorEl}
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                marginTop: '48px',
+                maxHeight: '144px',
+                width: '128px',
+              },
+            }}
+          >
+            {props.options.map((option) => (
+              <MenuItem
+                key={option.id}
+                selected={option.selected}
+                onClick={() => handleOptionAction(option.action)}
+              >
+                {option.name}
+              </MenuItem>
+            ))}
+          </Menu>
+        </Grid>
       </Grid>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
+      {/* TODO: check collapse error */}
+      <Collapse ref={wrapper} in={expanded} timeout='auto' unmountOnExit>
         <Box className={classes.content}>{props.children}</Box>
       </Collapse>
     </Box>
@@ -96,6 +183,8 @@ WrapperWidgetUI.defaultProps = {
 WrapperWidgetUI.propTypes = {
   title: PropTypes.string.isRequired,
   expandable: PropTypes.bool,
+  actions: PropTypes.array, // TODO: validate array items format
+  options: PropTypes.array, // TODO: validate array items format
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element.isRequired,
