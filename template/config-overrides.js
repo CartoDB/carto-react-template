88bf0680782c49572b6bb96dea1cd85e3c44ac54
Raúl Yeguas <neokore@gmail.com>
2020-10-26 18:24:32 +0100

    Fix theming failing with external libraries

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -4,7 +4,11 @@ const path = require('path');
 module.exports = function override(config, env) {
   const newConfig = config;
 
-  newConfig.resolve.alias.react = path.resolve('./node_modules/react');
+  newConfig.resolve.alias = {
+    ...newConfig.resolve.alias,
+    react: path.resolve('./node_modules/react'),
+    '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
+  };
 
   return newConfig;
 };
