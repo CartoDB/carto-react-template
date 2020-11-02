7c515501825814a2aab566950c9f71cebb05191e
Alberto Asuero <albertoasuero@gmail.com>
2020-11-02 11:01:10 +0100

    Simpilfy KPI and favicon

--- template/src/components/views/kpi/Kpi.js
+++ template/src/components/views/kpi/Kpi.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Outlet } from 'react-router-dom';
-import { addDataSource, removeLayer } from 'config/cartoSlice';
+import { Divider } from '@material-ui/core';
+import { AggregationTypes } from 'lib/sdk';
+import { CategoryWidget } from 'components/common/widgets/CategoryWidget';
+import { FormulaWidget } from 'components/common/widgets/FormulaWidget';
+import { setViewState, addDataSource, addLayer, removeLayer } from 'config/cartoSlice';
+import { currencyFormatter } from 'utils/numberFormatters';
 
 function Kpi() {
   const dispatch = useDispatch();
@@ -23,7 +27,46 @@ function Kpi() {
     };
   });
 
-  return <Outlet />;
+  useEffect(() => {
+    dispatch(
+      setViewState({
+        latitude: 31.802892,
+        longitude: -103.007813,
+        zoom: 3,
+        transitionDuration: 500,
+      })
+    );
+    dispatch(
+      addLayer({
+        id: 'revenueByStateLayer',
+        source: 'revenueByStateSource',
+        selectedStore: null,
+      })
+    );
+  });
+
+  return (
+    <div>
+      <FormulaWidget
+        title='Total revenue'
+        data-source='revenueByStateSource'
+        operation-column='revenue'
+        operation={AggregationTypes.SUM}
+        formatter={currencyFormatter}
+        viewport-filter
+      ></FormulaWidget>
+      <Divider />
+      <CategoryWidget
+        title='Revenue by state'
+        data-source='revenueByStateSource'
+        column='name'
+        operation-column='revenue'
+        operation={AggregationTypes.SUM}
+        formatter={currencyFormatter}
+        viewport-filter
+      />
+    </div>
+  );
 }
 
 export default Kpi;
