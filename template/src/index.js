205d75ebf5333df51353b9d5e3559798a0ad2a45
VictorVelarde <victor.velarde@gmail.com>
2020-11-03 16:41:43 +0100

    chore: remove react strict mode
    MaterialUI is currently no 100% compatible with React.StrictMode, as it relies in some (now) not recommended practices to find DOM nodes. That produces some console errors in chrome

--- template/src/index.js
+++ template/src/index.js
@@ -14,13 +14,11 @@ import store from './config/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
