ce7649b4f55e54f6cb97f7f269e867aa2dd99b30
VictorVelarde <victor.velarde@gmail.com>
2020-11-03 16:56:29 +0100

    refactor: make use of new scope to load datasets
    It avoids the previous "double oauth" request

--- template/src/components/views/Datasets.js
+++ template/src/components/views/Datasets.js
@@ -1,18 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography } from '@material-ui/core';
 
 import { getDatasets } from 'lib/sdk';
 
-import useOAuthLogin from 'components/common/oauth/useOAuthLogin';
-import {
-  setOAuthApp,
-  setTokenAndUserInfoAsync,
-  setError,
-  selectCredentials,
-} from 'config/oauthSlice';
+import { selectCredentials } from 'config/oauthSlice';
 
 import DatasetsList from 'components/views/DatasetsList';
 
@@ -37,23 +31,10 @@ const datasetsPagination = { page: 1, size: 50 };
 
 function Datasets() {
   const credentials = useSelector(selectCredentials);
-  const oauthApp = useSelector((state) => state.oauth.oauthApp);
 
-  // const [oauth, setOauth] = useState(oauthApp);
-  const [handleLogin] = useOAuthLogin(oauthApp, onParamsRefreshed);
-
-  const dispatch = useDispatch();
   const classes = useStyles();
 
   const [datasets, setDatasets] = useState([]);
-  const [extraPermissionRequired, setExtraPermissionRequired] = useState(true);
-
-  const getDataSetScopes = useCallback(() => {
-    const datasetScopes = datasets.map(
-      (dataset) => `datasets:r:${dataset.table_schema}.${dataset.name}`
-    );
-    return datasetScopes;
-  }, [datasets]);
 
   useEffect(() => {
     if (credentials) {
@@ -68,45 +49,6 @@ function Datasets() {
       });
     }
   }, [credentials]);
-
-  // temporary. Once we have r:* scope, this and next effect won't be necessary
-  useEffect(() => {
-    if (datasets.length > 0 && extraPermissionRequired) {
-      // Require extra permissions to load each dataset
-      const upToDateScopes = new Set(
-        (oauthApp.scopes ? [...oauthApp.scopes] : []).concat(getDataSetScopes())
-      );
-
-      const newOAuth = { ...oauthApp, scopes: [...upToDateScopes] };
-      dispatch(setOAuthApp(newOAuth));
-    }
-  });
-
-  // temporary.
-  useEffect(() => {
-    const oauthAlreadyUpdated = getDataSetScopes().every((s) =>
-      oauthApp.scopes.includes(s)
-    );
-    if (datasets.length > 0 && extraPermissionRequired && oauthAlreadyUpdated) {
-      // Handle a new popup window to authorize reading the datasets
-      handleLogin();
-      setExtraPermissionRequired(false);
-    }
-  }, [
-    getDataSetScopes,
-    datasets.length,
-    extraPermissionRequired,
-    oauthApp.scopes,
-    handleLogin,
-  ]);
-
-  function onParamsRefreshed(oauthParams) {
-    if (oauthParams.error) {
-      dispatch(setError(oauthParams));
-    } else {
-      dispatch(setTokenAndUserInfoAsync(oauthParams));
-    }
-  }
 
   return (
     <div className={classes.root}>
