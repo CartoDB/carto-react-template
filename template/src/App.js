fdb3f9af9342a5dcf5fba1e3dca1b5f5afb48666
VictorVelarde <victor.velarde@gmail.com>
2020-11-03 16:19:10 +0100

    fix: default route

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useRoutes, useNavigate } from 'react-router-dom';
+import { useRoutes, useNavigate, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import { createTheme } from '@carto/react-airship-ui';
 import routes from './routes';
@@ -9,10 +9,13 @@ const theme = createTheme();
 function App() {
   const routing = useRoutes(routes);
   const navigate = useNavigate();
+  const currentLocation = useLocation();
 
   useEffect(() => {
-    navigate('/stores');
-  }, [navigate]);
+    if (currentLocation.pathname === '/') {
+      navigate('/stores');
+    }
+  }, [navigate, currentLocation]);
 
   return <ThemeProvider theme={theme}>{routing}</ThemeProvider>;
 }
