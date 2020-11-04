4ec702dfca5ee9398f927038023a7b118dc7aa03
Alberto Asuero <albertoasuero@gmail.com>
2020-11-04 12:02:33 +0100

    Improve default router management

--- template/src/routes.js
+++ template/src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Home from 'components/views/Home';
 import Stores from 'components/views/stores/Stores';
 import StoresDetail from 'components/views/stores/StoresDetail';
+import { Navigate } from 'react-router-dom';
+
 import Kpi from 'components/views/Kpi';
 
 const routes = [
@@ -9,6 +11,7 @@ const routes = [
     path: '/',
     element: <Home />,
     children: [
+      { path: '/', element: <Navigate to='/stores' /> },
       { path: '/stores', element: <Stores /> },
       { path: '/stores/:id', element: <StoresDetail /> },
       { path: '/kpi', element: <Kpi /> },
