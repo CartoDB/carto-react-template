e2d8c3200eb50d1d63baac1d820f6b04e715baf1
Alberto Asuero <1161870+alasarr@users.noreply.github.com>
2020-11-04 12:06:08 +0100

    Improve default router management (#50)

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useRoutes, useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import { createTheme } from '@carto/react-airship-ui';
 import routes from './routes';
@@ -8,15 +8,6 @@ const theme = createTheme();
 
 function App() {
   const routing = useRoutes(routes);
-  const navigate = useNavigate();
-  const currentLocation = useLocation();
-
-  useEffect(() => {
-    if (currentLocation.pathname === '/') {
-      navigate('/stores');
-    }
-  }, [navigate, currentLocation]);
-
   return <ThemeProvider theme={theme}>{routing}</ThemeProvider>;
 }
 
