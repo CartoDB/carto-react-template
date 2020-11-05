98b2481707748442fc983b2671e483f0e8c62c2a
Raúl Yeguas <neokore@gmail.com>
2020-11-05 16:32:05 +0100

    Activate loading state for widgets (#57)

--- template/src/lib/sdk/widgets/FormulaWidget.js
+++ template/src/lib/sdk/widgets/FormulaWidget.js
@@ -6,6 +6,7 @@ import { getValue } from 'lib/sdk';
 
 export default function FormulaWidget(props) {
   const [formulaData, setFormulaData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const viewport = useSelector(
     (state) => props['viewport-filter'] && state.carto.viewport
   );
@@ -20,16 +21,18 @@ export default function FormulaWidget(props) {
       credentials &&
       (!props['viewport-filter'] || (props['viewport-filter'] && viewport))
     ) {
-      getValue({ ...props, data, filters, credentials, viewport }).then(
-        (data) => data && data[0] && setFormulaData(data[0].value)
-      );
+      setLoading(true);
+      getValue({ ...props, data, filters, credentials, viewport }).then((data) => {
+        data && data[0] && setFormulaData(data[0].value);
+        setLoading(false);
+      });
     } else {
       setFormulaData(null);
     }
   }, [credentials, data, filters, viewport, props]);
 
   return (
-    <WrapperWidgetUI title={props.title} expandable={true}>
+    <WrapperWidgetUI title={props.title} expandable={true} loading={loading}>
       <FormulaWidgetUI data={formulaData} formatter={props.formatter} />
     </WrapperWidgetUI>
   );
