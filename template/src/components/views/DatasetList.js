bdc6c572c22778a7908877d5d1fd419f51824798
VictorVelarde <victor.velarde@gmail.com>
2020-11-05 14:05:20 +0100

    feat: remove dialog confirmation before dataset loading

--- template/src/components/views/DatasetList.js
+++ template/src/components/views/DatasetList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { addLayer, addSource, removeLayer, removeSource } from 'config/cartoSlice';
@@ -6,12 +6,6 @@ import { selectOAuthCredentials } from 'config/oauthSlice';
 
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
   IconButton,
   List,
   ListItem,
@@ -37,22 +31,10 @@ function DatasetList(props) {
   const { oauthLayer } = useSelector((state) => state.carto.layers);
 
   const dispatch = useDispatch();
-  const [open, setOpen] = useState(false);
-  const [selectedDataset, setSelectedDataset] = useState({});
   const classes = useStyles();
 
-  const handleClickOpen = (dataset) => {
-    setSelectedDataset(dataset);
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   // Load dataset & layer to store (so to Map)
-  const loadDataset = () => {
-    handleClose();
-
+  const loadDataset = (selectedDataset) => {
     const { name: datasetName, table_schema: schema } = selectedDataset;
     const dataSourceCredentials = { ...credentials, username: schema };
 
@@ -78,83 +60,50 @@ function DatasetList(props) {
   }
 
   return (
-    <>
-      <List>
-        {props.datasets.map((dataset) => {
-          const labelId = `checkbox-list-label-${dataset.name}`;
+    <List>
+      {props.datasets.map((dataset) => {
+        const labelId = `checkbox-list-label-${dataset.name}`;
 
-          const datasetLoaded = oauthLayer && oauthLayer.name === dataset.name;
-          // const secondary = `@${dataset.table_schema} (${dataset.privacy})`;
-          const secondary = `${dataset.privacy}`;
+        const datasetLoaded = oauthLayer && oauthLayer.name === dataset.name;
+        // const secondary = `@${dataset.table_schema} (${dataset.privacy})`;
+        const secondary = `${dataset.privacy}`;
 
-          return (
-            <ListItem key={dataset.name} divider role={undefined}>
-              <div className={classes.dataset}>
-                <ListItemText
-                  id={labelId}
-                  primary={
-                    <Typography className={classes.datasetName}>
-                      {dataset.name}
-                    </Typography>
-                  }
-                  secondary={secondary}
-                />
-              </div>
-              <ListItemSecondaryAction>
-                {datasetLoaded ? (
-                  <IconButton
-                    edge='end'
-                    aria-label='remove dataset'
-                    onClick={() => removeDataset()}
-                  >
-                    {/* Remove dataset */}
-                    <HighlightOff color='primary' />
-                  </IconButton>
-                ) : (
-                  <IconButton
-                    edge='end'
-                    aria-label='add dataset'
-                    onClick={() => handleClickOpen(dataset)}
-                  >
-                    {/* Load dataset */}
-                    <ChevronRight color='primary' />
-                  </IconButton>
-                )}
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
-
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='alert-add-dataset-dialog'
-        aria-describedby='alert-add-dataset-description'
-      >
-        <DialogTitle id='alert-add-dataset-dialog'>{'Load dataset'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id='alert-add-dataset-description'>
-            You are going to load Dataset <strong>{selectedDataset.name}</strong>, are you
-            sure?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color='primary' disableElevation>
-            Cancel
-          </Button>
-          <Button
-            onClick={loadDataset}
-            variant='contained'
-            color='primary'
-            autoFocus
-            disableElevation
-          >
-            OK
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+        return (
+          <ListItem key={dataset.name} divider role={undefined}>
+            <div className={classes.dataset}>
+              <ListItemText
+                id={labelId}
+                primary={
+                  <Typography className={classes.datasetName}>{dataset.name}</Typography>
+                }
+                secondary={secondary}
+              />
+            </div>
+            <ListItemSecondaryAction>
+              {datasetLoaded ? (
+                <IconButton
+                  edge='end'
+                  aria-label='remove dataset'
+                  onClick={() => removeDataset()}
+                >
+                  {/* Remove dataset */}
+                  <HighlightOff color='primary' />
+                </IconButton>
+              ) : (
+                <IconButton
+                  edge='end'
+                  aria-label='add dataset'
+                  onClick={() => loadDataset(dataset)}
+                >
+                  {/* Load dataset */}
+                  <ChevronRight color='primary' />
+                </IconButton>
+              )}
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 }
 
