39ecd1944d12e337fa204ffd4c344ec4d1eb3049
Alberto Asuero <albertoasuero@gmail.com>
2020-11-06 13:47:03 +0100

    Histogram discrete

--- template/src/lib/sdk/models/HistogramModel.js
+++ template/src/lib/sdk/models/HistogramModel.js
@@ -1,32 +1,37 @@
 import { executeSQL, getFilterCondition, getConditionFromViewPort } from '..';
 
 export const getHistogram = (props) => {
-  const {
-    data,
-    credentials,
-    operation,
-    'operation-column': operationColumn = '*',
-    column,
-    filters,
-    viewport,
-  } = props;
+  const { data, credentials, column, operation, ticks, filters, viewport } = props;
+
+  const operationColumn = props['operation-column'] || column;
 
   if (Array.isArray(data)) {
-    throw new Error('Array is not a valid type to get categories');
+    throw new Error('Array is not a valid type to get histogram');
   }
 
   let query =
     (viewport &&
-      `SELECT * FROM (${data}) as q WHERE ${getConditionFromViewPort(viewport)}`) ||
+      `SELECT * FROM (${data})  as q WHERE ${getConditionFromViewPort(viewport)}`) ||
     data;
 
-  console.log(filters);
-
-  query = `
-    SELECT ${operation}(${operationColumn}) as value, ${column} as tick
-    FROM (${query}) as q
-    ${getFilterCondition(filters)} group by ${column}
-  `;
+  if (!ticks) {
+    query = `WITH all_ticks as (
+      SELECT ${column} as tick
+        FROM (${query}) as q
+      GROUP BY tick
+    ),
+    ticks as (
+      SELECT ${column} as tick, ${operation}(${operationColumn}) as value
+        FROM (${query}) as q
+      ${getFilterCondition(filters)}
+      GROUP BY tick
+    )
+    SELECT a.tick, b.value
+      FROM all_ticks a
+      LEFT JOIN ticks b ON a.tick=b.tick
+      ORDER BY value DESC NULLS LAST`;
+  } else {
+  }
 
   return executeSQL(credentials, query);
 };
