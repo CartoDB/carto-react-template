072d2f88ef03cd3154a34e525cc414481df88cef
Raúl Yeguas <neokore@gmail.com>
2020-11-12 11:19:32 +0100

    Fix storybook

--- template/src/lib/ui/.storybook/main.js
+++ template/src/lib/ui/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
 };
