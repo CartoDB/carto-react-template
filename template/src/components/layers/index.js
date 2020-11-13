4b13609d2a33640d3de05a824fe047f466baec8e
Alberto Arana <aaranaboza@gmail.com>
2020-11-13 17:52:38 +0100

    clear code

--- template/src/components/layers/index.js
+++ template/src/components/layers/index.js
@@ -3,7 +3,6 @@ import StoresLayer from './StoresLayer';
 import OAuthLayer from './OAuthLayer';
 import GeocoderLayer from './GeocoderLayer';
 import IsochroneLayer from './IsochroneLayer';
-import ExampleLayer from './ExampleLayer';
 // Auto import
 
 export const getLayers = () => {
