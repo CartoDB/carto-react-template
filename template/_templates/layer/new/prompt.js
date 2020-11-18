ea1a6342c86ca29cd3426dd92ce6dc2c3c8c6ac7
Alberto Arana <aaranaboza@gmail.com>
2020-11-18 09:05:14 +0100

    improved hygen

--- template/_templates/layer/new/prompt.js
+++ template/_templates/layer/new/prompt.js
@@ -6,14 +6,18 @@ const path = require('path');
 const { cwd } = require('process');
 const { promptArgs } = require('../../promptUtils');
 
+const TYPES_SOURCE = ['sql', 'bq'];
+
 const TYPES_LAYER = {
-  sql: {
+  [TYPES_SOURCE[0]]: {
     title: 'SQL dataset',
     className: 'CartoSQLLayer',
+    msg: 'Type a query',
   },
-  bq: {
+  [TYPES_SOURCE[1]]: {
     title: 'BigQuery Tileset',
     className: 'CartoBQTilerLayer',
+    msg: 'Type the name of your tileset',
   },
 };
 
@@ -33,20 +37,23 @@ const prompt = async ({ prompter, args }) => {
       type: 'select',
       name: 'type',
       message: 'Choose type',
-      choices: [TYPES_LAYER['sql'], TYPES_LAYER['bq']],
+      choices: [...Object.values(TYPES_LAYER)],
     },
   ]);
 
   let answers = await promptArgs({ prompter, args, questions });
 
-  answers.type_source = TYPES_LAYER['sql'].title === answers.type ? 'sql' : 'bq';
+  answers.type_source =
+    TYPES_LAYER[TYPES_SOURCE[0]].title === answers.type
+      ? TYPES_SOURCE[0]
+      : TYPES_SOURCE[1];
+  answers.type_className = TYPES_LAYER[answers.type_source].className;
 
   questions = [
     {
       type: 'input',
       name: 'data',
-      message:
-        answers.type_source === 'sql' ? 'Type a query' : 'Type the name of your tileset',
+      message: TYPES_LAYER[answers.type_source].msg,
     },
   ];
 
@@ -54,46 +61,6 @@ const prompt = async ({ prompter, args }) => {
     ...answers,
     ...(await promptArgs({ prompter, args: answers, questions })),
   };
-
-  questions = [
-    {
-      type: 'confirm',
-      name: 'attach',
-      message: 'Do you want to attach to some view',
-    },
-  ];
-
-  answers = {
-    ...answers,
-    ...(await promptArgs({ prompter, args: answers, questions })),
-  };
-
-  if (answers.attach) {
-    questions = [
-      {
-        type: 'input',
-        name: 'view',
-        message: 'View name: ',
-      },
-    ];
-    answers = {
-      ...answers,
-      ...(await promptArgs({ prompter, args: answers, questions })),
-    };
-
-    const viewFile = path.join(cwd(), 'src', 'components', 'views', `${answers.view}.js`);
-
-    const existView = await new Promise((resolve) => {
-      fs.access(viewFile, fs.F_OK, (err) => {
-        if (err) resolve(false);
-        return resolve(true);
-      });
-    });
-
-    if (!existView) {
-      throw new Error(`The view doesn't exist`);
-    }
-  }
 
   questions = [
     {
--- template/_templates/promptUtils.js
+++ template/_templates/promptUtils.js
@@ -1,8 +1,4 @@
 async function promptArgs({ prompter, args, questions }) {
-  // const providedArgs = questions.reduce((selectedArgs, question) => {
-  //   if (args[question.name]) selectedArgs[question.name] = args[question.name];
-  //   return selectedArgs;
-  // }, {});
   const answers = await prompter.prompt(questions.filter(({ name }) => !args[name]));
   return { ...answers, ...args };
 }
