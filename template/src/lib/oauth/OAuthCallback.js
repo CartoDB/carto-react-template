e389a42a9fe5c2b00f0c21db7aedcf75b04335e3
VictorVelarde <victor.velarde@gmail.com>
2020-11-18 22:51:43 +0100

    refactor: delete local lib folder

--- template/src/lib/oauth/OAuthCallback.js
+++
@@ -1,6 +0,0 @@
-/**
- * Component to attend OAuth callbacks on /oauthCallback
- */
-export default function OAuthCallback() {
-  return null;
-}
--- template/src/lib/oauth/index.js
+++
@@ -1,8 +0,0 @@
-export {
-  createOAuthPopup,
-  getOAuthParamsFromCallback,
-  buildTokenRenewalUrl,
-} from './oauthHelper';
-export { default as OAuthCallback } from './OAuthCallback';
-export { default as OAuthLogin } from './OAuthLogin';
-export { default as useOAuthLogin } from './useOAuthLogin';
