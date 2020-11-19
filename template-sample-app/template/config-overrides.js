457bdb978f8ab826c8a57d41d0e4f0519595f35a
Alberto Asuero <1161870+alasarr@users.noreply.github.com>
2020-11-19 14:08:44 +0100

    Fix: Google Maps (#109)

--- template-sample-app/template/config-overrides.js
+++ template-sample-app/template/config-overrides.js
@@ -9,6 +9,8 @@ module.exports = function override(config, env) {
     react: path.resolve('./node_modules/react'),
     'react-redux': path.resolve('./node_modules/react-redux'),
     '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
+    '@deck.gl/core': path.resolve('./node_modules/@deck.gl/core'),
+    '@deck.gl/google-maps': path.resolve('./node_modules/@deck.gl/google-maps'),
   };
 
   return newConfig;
