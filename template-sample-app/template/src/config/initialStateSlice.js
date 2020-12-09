f504551f3b20830279fbabc1bcb2e8a2c7a9c884
VictorVelarde <victor.velarde@gmail.com>
2020-12-09 22:26:48 +0100

    doc: add refactor & fix note to changelog

--- template-sample-app/template/src/config/initialStateSlice.js
+++ template-sample-app/template/src/config/initialStateSlice.js
@@ -24,7 +24,7 @@ export const oauthInitialState = {
     scopes: [
       'user:profile', // to load avatar photo
       'datasets:metadata', // to list all your datasets,
-      'datasets:r:*', // to load any of your datasets as a layer
+      'datasets:r:*', // to read any of your datasets
       'dataservices:geocoding', // to use geocoding through Data Services API
       'dataservices:isolines', // to launch isochrones or isodistances through Data Services API
     ],
