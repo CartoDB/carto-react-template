c9ca352d6d896ff1888af9f5ab43ec090e4dee73
Raúl Yeguas <neokore@gmail.com>
2020-12-09 16:55:06 +0100

    Collapsable legend for mobile

--- template-sample-app/template/src/components/legends/Legend.js
+++ template-sample-app/template/src/components/legends/Legend.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import KpiLegend from './KpiLegend';
 import StoresLegend from './StoresLegend';
-import { Paper, makeStyles } from '@material-ui/core';
+import { Paper, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
+import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,14 +14,40 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     },
   },
+  legendButton: {
+    padding: theme.spacing(0.75),
+  },
+  legendIcon: {
+    display: 'block',
+  },
 }));
 
 export function Legend(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+  const [collapsed, setCollapsed] = useState(true);
   return (
-    <Paper elevation={4} className={`${classes.root} ${props.className} `}>
-      <KpiLegend />
-      <StoresLegend />
-    </Paper>
+    <>
+      {isMobile && collapsed && (
+        <Paper
+          elevation={4}
+          className={`${classes.legendButton} ${props.className}`}
+          onClick={() => setCollapsed(false)}
+        >
+          <ListAltOutlinedIcon className={classes.legendIcon} alt='Toggle legend' />
+        </Paper>
+      )}
+      {((isMobile && !collapsed) || !isMobile) && (
+        <Paper
+          elevation={4}
+          className={`${classes.root} ${props.className} `}
+          onClick={() => setCollapsed(true)}
+        >
+          <KpiLegend />
+          <StoresLegend />
+        </Paper>
+      )}
+    </>
   );
 }
