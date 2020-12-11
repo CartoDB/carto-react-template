ead345c8ae1a8ade9d9bf57e5273ef4012a8d49c
Raúl Yeguas <neokore@gmail.com>
2020-12-11 16:38:14 +0100

    Fix geocoder margins

--- template-skeleton/template/src/components/views/Main.js
+++ template-skeleton/template/src/components/views/Main.js
@@ -71,11 +71,11 @@ const useStyles = makeStyles((theme) => ({
   },
   geocoder: {
     position: 'absolute',
-    top: theme.spacing(3),
-    left: theme.spacing(3),
+    top: theme.spacing(2),
+    left: theme.spacing(2),
 
     [theme.breakpoints.down('xs')]: {
-      width: `calc(100% - ${theme.spacing(6)}px)`,
+      width: `calc(100% - ${theme.spacing(4)}px)`,
     },
   },
 }));
