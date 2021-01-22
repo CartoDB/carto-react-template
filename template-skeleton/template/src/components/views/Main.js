23bb24bf0cde0f7395fafd13d99ffc89bc8400d4
Sergio Clebal <sergioclebal@gmail.com>
2021-01-22 09:21:32 +0100

    Refactored data & source structure in template-skeleton.

--- template-skeleton/template/src/components/views/Main.js
+++ template-skeleton/template/src/components/views/Main.js
@@ -166,6 +166,8 @@ export default function Main() {
   const mobileContainer = React.useRef(null);
   const desktopContainer = React.useRef(null);
 
+  // Auto import useEffect
+
   const handleClose = () => {
     dispatch(setError(null));
   };
---
+++ template-skeleton/template/src/data/sources/Source.js
@@ -0,0 +1,7 @@
+export const SOURCE_ID = 'source';
+
+export const source = {
+  id: SOURCE_ID,
+  data: `TYPE YOUR QUERY HERE`,
+  type: 'sql',
+};
