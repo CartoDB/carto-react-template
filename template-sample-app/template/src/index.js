b122da95d75ac14efbfafe240ee4064cd5863bc3
Alberto Arana <aaranaboza@gmail.com>
2021-01-27 11:20:29 +0100

    Updated to CRA4 and React 17

--- template-sample-app/template/src/index.js
+++ template-sample-app/template/src/index.js
@@ -13,7 +13,8 @@ import App from './App';
 
 import { initialState, oauthInitialState } from 'config/initialStateSlice';
 import configureAppStore from './config/store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 import { createCartoSlice, createOauthCartoSlice } from '@carto/react/redux';
 
@@ -33,5 +34,10 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
