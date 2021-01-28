7aaa85f0c3064affefd704537bed83234a94d375
Sergio Clebal <sergioclebal@gmail.com>
2021-01-28 16:49:32 +0100

    Fix linter in source.

--- template-sample-app/template/src/data/sources/tilesetSource.js
+++ template-sample-app/template/src/data/sources/tilesetSource.js
@@ -1,7 +1,9 @@
 const TILESET_SOURCE_ID = 'tilesetSource';
 
-export default {
+const tilesetSource = {
   id: TILESET_SOURCE_ID,
   type: 'bq',
   data: 'cartobq.maps.osm_buildings',
 }
+
+export default tilesetSource;
