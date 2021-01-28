429201e9f0f1d1c1b1ca81e26e6eb3e0d603c978
AdriSolid <biotekgis@gmail.com>
2021-01-28 11:57:33 +0100

    tests: add basic e2e tests for Tileset page

---
+++ template-sample-app/template/cypress/integration/tileset_page_spec.js
@@ -0,0 +1,38 @@
+const config = require('./config');
+
+const TILESET_PAGE = `${config.ROOT_PAGE}/${config.TILESET_PATH}`;
+
+describe('Tileset Page', () => {
+  it('successfully loads', () => {
+    cy.visit(TILESET_PAGE);
+  });
+
+  const getAggregationSumWidget = () => {
+    return cy.findByRole('region', { name: 'Total aggregated sum' });
+  };
+
+  const getAggregationCountWidget = () => {
+    return cy.findByRole('region', { name: 'Total aggregated count' });
+  };
+
+  it('widgets are visible', () => {
+    cy.visit(TILESET_PAGE);
+
+    getAggregationSumWidget().should('exist');
+    getAggregationCountWidget().should('exist');
+  });
+
+  it('calculations are working fine', () => {
+    cy.visit(TILESET_PAGE);
+
+    // Note: calculations depend on viewport
+    cy.viewport(1000, 660);
+
+    // Zoom out, to fit all layer
+    const zoomOut = cy.findByRole('button', { name: /Decrease zoom/i });
+    zoomOut.click();
+
+    // Total aggregated sum
+    getAggregationSumWidget().findByText('411M').should('exist');
+  });
+});
