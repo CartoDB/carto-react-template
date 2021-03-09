54066738f40f217e150bccc224423eb2b49006ba
AdriSolid <biotekgis@gmail.com>
2021-03-09 14:02:41 +0100

    fix: viewportFilter prop as true by default

--- template-sample-app/template/src/components/views/Tileset.js
+++ template-sample-app/template/src/components/views/Tileset.js
@@ -76,7 +76,6 @@ function Tileset() {
         operation={AggregationTypes.SUM}
         formatter={numberFormatter}
         onError={onTotalFareAmountWidgetError}
-        viewportFilter
       />
 
       <Divider />
@@ -86,10 +85,10 @@ function Tileset() {
         title='Total aggregated count'
         dataSource={tilesetSource.id}
         xAxisFormatter={numberFormatter}
+        s
         operation={AggregationTypes.COUNT}
         column='aggregated_total'
         ticks={[10, 100, 1e3, 1e4, 1e5, 1e6]}
-        viewportFilter
       />
 
       <Divider />
