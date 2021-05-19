4fffbbc591f47f26b1af965d6bd783fe486bee4b
Alberto Asuero <albertoasuero@gmail.com>
2021-05-19 16:06:58 +0200

    Bootstrap script and sample app adaptation

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+set_template()
+{
+  echo "Setup $1"
+  cp -R hygen/_templates/ "$1"/template/_templates
+  cd "$1"/template
+  ln -sf package.dev.json package.json
+  yarn 
+  yarn link-carto-react
+  cd -
+}
+
+set_template template-sample-app
+set_template template-skeleton
+set_template template-skeleton-cn
+
