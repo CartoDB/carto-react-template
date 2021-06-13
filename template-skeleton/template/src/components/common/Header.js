9fc0b1e5b7c0c9b730cd0257b38e83a3401008c1
Alberto Arana <aaranadev@gmail.com>
2021-06-13 21:39:38 +0200

    create context component in Header

--- template-skeleton/template/src/components/common/Header.js
+++ template-skeleton/template/src/components/common/Header.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   AppBar,
   Drawer,
@@ -17,46 +17,100 @@ import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 import UserMenu from 'components/views/login/UserMenu';
 import { NavLink, useLocation } from 'react-router-dom';
-import cartoLogo from 'assets/img/carto-logo.svg';
-import cartoLogoXs from 'assets/img/carto-logo-xs.svg';
+import { ReactComponent as CartoLogo } from 'assets/img/carto-logo.svg';
+import { ReactComponent as CartoLogoXS } from 'assets/img/carto-logo-xs.svg';
 
-const useStyles = makeStyles((theme) => ({
-  header: {
-    boxShadow: 'none',
-    zIndex: theme.zIndex.modal + 1,
-    overflow: 'hidden',
-  },
-  divider: {
-    margin: theme.spacing(0, 3),
-  },
-  menuButton: {
-    margin: theme.spacing(0, 0.75, 0, -1.25),
-
-    '& + hr': {
-      marginRight: theme.spacing(1.5),
-    },
-  },
-  drawer: {
-    minWidth: 260,
-  },
+const useStylesCommon = makeStyles((theme) => ({
   title: {
     '& h1': {
       fontWeight: theme.typography.fontWeightRegular,
-      color: theme.palette.common.white,
+      color: theme.palette.appBar?.contrastText || theme.palette.primary?.contrastText,
 
-      '& img': {
+      '& svg': {
         height: `${theme.typography.subtitle1.lineHeight}em`,
-        marginRight: theme.spacing(1.5),
+        marginRight: theme.spacing(2),
+        width: 'auto',
         verticalAlign: 'bottom',
       },
     },
   },
 }));
 
+const useStyles = makeStyles((theme) => {
+  return {
+    navBar: {
+      zIndex: theme.zIndex.modal + 1,
+      overflow: 'hidden',
+      backgroundColor: theme.palette.appBar?.main || theme.palette.primary.main,
+    },
+    navLink: {
+      color: theme.palette.appBar?.contrastText || theme.palette.primary?.contrastText,
+    },
+  };
+});
+
 export default function Header() {
+  const classes = useStyles();
+  return (
+    <AppBar position='static' className={classes.navBar}>
+      <Toolbar variant='dense'>
+        <Desktop />
+        <Mobile />
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+const useStylesDesktop = makeStyles((theme) => ({
+  divider: {
+    margin: theme.spacing(0, 3),
+  },
+}));
+
+function Desktop() {
+  const location = useLocation();
+  const classes = {
+    ...useStylesCommon(),
+    ...useStylesDesktop(),
+  };
+
+  const pathname = location.pathname.split('/')[1];
+
+  return (
+    <Hidden xsDown>
+      <Link component={NavLink} to='/' className={classes.title}>
+        <Typography component='h1' variant='subtitle1' noWrap>
+          <CartoLogo />
+          <AppName />
+        </Typography>
+      </Link>
+      <Divider orientation='vertical' className={classes.divider} light></Divider>
+      <NavigationMenu pathname={pathname} />
+      <Grid container item xs justify='flex-end'>
+        <UserMenu />
+      </Grid>
+    </Hidden>
+  );
+}
+
+const useStylesMobile = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  drawer: {
+    minWidth: 260,
+  },
+}));
+
+function Mobile() {
   const location = useLocation();
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const classes = useStyles();
+  const classes = {
+    ...useStylesCommon(),
+    ...useStylesMobile(),
+  };
+
+  const pathname = location.pathname.split('/')[1];
 
   useEffect(() => {
     setDrawerOpen(false);
@@ -67,71 +121,67 @@ export default function Header() {
   };
 
   return (
-    <AppBar position='static' className={classes.header}>
-      <Toolbar variant='dense'>
-        <Hidden smUp>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-            onClick={handleDrawerToggle}
-          >
-            {drawerOpen ? <CloseIcon /> : <MenuIcon />}
-          </IconButton>
-          <Divider orientation='vertical' light />
-        </Hidden>
-        <Link component={NavLink} to='/' className={classes.title}>
-          <Typography component='h1' variant='subtitle1' noWrap>
-            <Hidden xsDown>
-              <img src={cartoLogo} alt='CARTO ' />
-            </Hidden>
-            <Hidden smUp>
-              <img src={cartoLogoXs} alt='CARTO ' />
-            </Hidden>
-            <strong>React</strong> Demo
-          </Typography>
-        </Link>
-        <Hidden xsDown>
-          <Divider orientation='vertical' className={classes.divider} light />
-          <NavigationMenu location={location} />
-        </Hidden>
-        <Hidden smUp>
-          <Drawer
-            variant='temporary'
-            anchor='left'
-            open={drawerOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            PaperProps={{
-              className: classes.drawer,
-            }}
-          >
-            <Toolbar variant='dense' />
-            <Grid container direction='column' justify='space-between' item xs>
-              <NavigationMenu location={location} column={true} />
-            </Grid>
-          </Drawer>
-        </Hidden>
-        <Grid container item xs justify='flex-end'>
-          <UserMenu />
+    <Hidden smUp>
+      <IconButton
+        edge='start'
+        className={classes.menuButton}
+        color='inherit'
+        aria-label='menu'
+        onClick={handleDrawerToggle}
+      >
+        {drawerOpen ? <CloseIcon /> : <MenuIcon />}
+      </IconButton>
+      <Link component={NavLink} to='/' className={classes.title}>
+        <Typography component='h1' variant='subtitle1' noWrap>
+          <Hidden smUp>
+            <CartoLogoXS />
+            <Divider orientation='vertical' light />
+          </Hidden>
+          <AppName />
+        </Typography>
+      </Link>
+      <Drawer
+        variant='temporary'
+        anchor='left'
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        PaperProps={{
+          className: classes.drawer,
+        }}
+      >
+        <Toolbar variant='dense' />
+        <Grid container direction='column' justify='space-between' item xs>
+          <NavigationMenu pathname={pathname} vertical />
         </Grid>
-      </Toolbar>
-    </AppBar>
+      </Drawer>
+      <Grid container item xs justify='flex-end'>
+        <UserMenu />
+      </Grid>
+    </Hidden>
+  );
+}
+
+function AppName() {
+  return (
+    <>
+      <strong>React</strong> Demo
+    </>
   );
 }
 
 const useStylesNavigationMenu = makeStyles((theme) => ({
   navTabs: {
     '& .MuiTabs-indicator': {
-      backgroundColor: theme.palette.common.white,
+      backgroundColor:
+        theme.palette.appBar?.contrastText || theme.palette.primary?.contrastText,
     },
   },
 }));
 
-const NavigationMenu = ({ location, column: vertical }) => {
+function NavigationMenu({ pathname, vertical }) {
   const classes = useStylesNavigationMenu();
 
   return (
@@ -141,8 +191,8 @@ const NavigationMenu = ({ location, column: vertical }) => {
       className={!vertical ? classes.navTabs : ''}
     >
       <Tabs
-        value={location.pathname.split('/')[1] || ''}
-        textColor={vertical ? 'primary' : 'inherit'}
+        value={pathname}
+        textColor='inherit'
         orientation={vertical ? 'vertical' : 'horizontal'}
         variant={vertical ? 'fullWidth' : 'standard'}
       >
@@ -151,4 +201,4 @@ const NavigationMenu = ({ location, column: vertical }) => {
       </Tabs>
     </Grid>
   );
-};
+}
