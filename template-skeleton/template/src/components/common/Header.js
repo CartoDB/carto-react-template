6e77df2d21d65278f0e1c5420fb76067348e63ba
Alberto Arana <aaranadev@gmail.com>
2021-06-17 17:34:13 +0200

    wip

--- template-skeleton/template/src/components/common/Header.js
+++ template-skeleton/template/src/components/common/Header.js
@@ -18,7 +18,28 @@ import CloseIcon from '@material-ui/icons/Close';
 import UserMenu from 'components/views/login/UserMenu';
 import { NavLink, useLocation } from 'react-router-dom';
 import cartoLogo from 'assets/img/carto-logo.svg';
-import cartoLogoXs from 'assets/img/carto-logo-xs.svg';
+import { ReactComponent as CartoLogoXS } from 'assets/img/carto-logo-xs.svg';
+
+const useStylesCommon = makeStyles((theme) => ({
+  title: {
+    '& h1': {
+      display: 'flex',
+      fontWeight: theme.typography.fontWeightRegular,
+      color: theme.palette.common.white,
+
+      '& strong': {
+        marginRight: theme.spacing(0.5),
+      },
+
+      '& svg': {
+        height: `${theme.typography.subtitle1.lineHeight}em`,
+        marginRight: theme.spacing(1.5),
+        width: 'auto',
+        verticalAlign: 'bottom',
+      },
+    },
+  },
+}));
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -58,47 +79,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const NavigationMenu = ({ location, column: vertical }) => {
-  const classes = useStyles();
-
-  const pathname = location.pathname.split('/')[1] || '';
-
-  return (
-    <Grid
-      container
-      direction={vertical ? 'column' : 'row'}
-      className={!vertical ? classes.navTabs : ''}
-    >
-      <Tabs
-        value={pathname}
-        textColor={vertical ? 'primary' : 'inherit'}
-        orientation={vertical ? 'vertical' : 'horizontal'}
-        variant={vertical ? 'fullWidth' : 'standard'}
-      >
-        <Tab label='Home' value='' component={NavLink} to='/' />
-        {/* [hygen] Import links */}
-      </Tabs>
-    </Grid>
-  );
-};
-
 export default function Header() {
-  const location = useLocation();
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  // const location = useLocation();
+  // const [drawerOpen, setDrawerOpen] = useState(false);
   const classes = useStyles();
 
-  useEffect(() => {
-    setDrawerOpen(false);
-  }, [location]);
+  // useEffect(() => {
+  //   setDrawerOpen(false);
+  // }, [location]);
 
-  const handleDrawerToggle = () => {
-    setDrawerOpen(!drawerOpen);
-  };
+  // const handleDrawerToggle = () => {
+  //   setDrawerOpen(!drawerOpen);
+  // };
 
   return (
     <AppBar position='static' className={classes.header}>
       <Toolbar variant='dense'>
-        <Hidden smUp>
+        <Mobile />
+        {/* <Hidden smUp>
           <IconButton
             edge='start'
             className={classes.menuButton}
@@ -109,8 +107,8 @@ export default function Header() {
             {drawerOpen ? <CloseIcon /> : <MenuIcon />}
           </IconButton>
           <Divider orientation='vertical' light />
-        </Hidden>
-        <Link component={NavLink} to='/' className={classes.title}>
+        </Hidden> */}
+        {/* <Link component={NavLink} to='/' className={classes.title}>
           <Typography component='h1' variant='subtitle1' noWrap>
             <Hidden xsDown>
               <img src={cartoLogo} alt='CARTO ' />
@@ -120,8 +118,8 @@ export default function Header() {
             </Hidden>
             <strong>React</strong> Demo
           </Typography>
-        </Link>
-        <Hidden xsDown>
+        </Link> */}
+        {/* <Hidden xsDown>
           <Divider orientation='vertical' className={classes.divider} light />
           <NavigationMenu location={location} />
         </Hidden>
@@ -146,8 +144,121 @@ export default function Header() {
         </Hidden>
         <Grid container item xs justify='flex-end'>
           <UserMenu />
-        </Grid>
+        </Grid> */}
       </Toolbar>
     </AppBar>
+  );
+}
+
+const useStylesMobile = makeStyles((theme) => ({
+  menuButton: {
+    margin: theme.spacing(0, 0.75, 0, -1.25),
+
+    '& + hr': {
+      marginRight: theme.spacing(1.5),
+    },
+  },
+  drawer: {
+    minWidth: 260,
+  },
+}));
+
+function Mobile() {
+  const location = useLocation();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const classes = {
+    ...useStylesCommon(),
+    ...useStylesMobile(),
+  };
+
+  useEffect(() => {
+    setDrawerOpen(false);
+  }, [location]);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <Hidden smUp>
+      <IconButton
+        className={classes.menuButton}
+        color='inherit'
+        aria-label='menu'
+        onClick={handleDrawerToggle}
+      >
+        {drawerOpen ? <CloseIcon /> : <MenuIcon />}
+      </IconButton>
+      <Divider orientation='vertical' light />
+      <Link component={NavLink} to='/' className={classes.title}>
+        <Typography component='h1' variant='subtitle1' noWrap>
+          <Hidden smUp>
+            <CartoLogoXS />
+            <Divider orientation='vertical' light />
+          </Hidden>
+          <AppName />
+        </Typography>
+      </Link>
+      <Drawer
+        variant='temporary'
+        anchor='left'
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        PaperProps={{
+          className: classes.drawer,
+        }}
+      >
+        <Toolbar variant='dense' />
+        <Grid container direction='column' justify='space-between' item xs>
+          <NavigationMenu column={true} />
+        </Grid>
+      </Drawer>
+    </Hidden>
+  );
+}
+
+function AppName() {
+  return (
+    <>
+      <strong>React</strong> Demo
+    </>
+  );
+}
+
+const useStylesNavigationMenu = makeStyles((theme) => ({
+  navTabs: {
+    '& .MuiTabs-indicator': {
+      backgroundColor:
+        theme.palette.appBar?.contrastText || theme.palette.primary?.contrastText,
+    },
+  },
+}));
+
+function NavigationMenu({ column: vertical }) {
+  const location = useLocation();
+  const classes = useStylesNavigationMenu();
+
+  const pathname = location.pathname.split('/')[1] || '';
+
+  return (
+    <Grid
+      container
+      direction={vertical ? 'column' : 'row'}
+      className={!vertical ? classes.navTabs : ''}
+    >
+      <Tabs
+        value={pathname}
+        textColor={vertical ? 'primary' : 'inherit'}
+        orientation={vertical ? 'vertical' : 'horizontal'}
+        variant={vertical ? 'fullWidth' : 'standard'}
+      >
+        <Tab label='Home' value='' component={NavLink} to='/' />
+        {/* [hygen] Import links */}
+      </Tabs>
+    </Grid>
   );
 }
