fa21951a85257399a16a2ec349ae9c97ace5491f
Alberto Arana <aaranadev@gmail.com>
2021-06-21 10:08:15 +0200

    Improved context in Main

--- template-skeleton/template/src/components/views/Main.js
+++ template-skeleton/template/src/components/views/Main.js
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -7,11 +6,8 @@ import {
   Fab,
   Grid,
   Hidden,
-  Portal,
   Snackbar,
   Toolbar,
-  useTheme,
-  useMediaQuery,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
@@ -32,9 +28,6 @@ const useStyles = makeStyles((theme) => ({
       flexShrink: 0,
     },
   },
-  drawerPaper: {
-    width: DRAWER_WIDTH,
-  },
   widgetDrawerToggle: {
     position: 'absolute',
     bottom: 0,
@@ -42,58 +35,6 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 1,
     textAlign: 'center',
   },
-  bottomSheet: {
-    maxHeight: `calc(100% - ${theme.spacing(6)}px)`,
-
-    '&$closed': {
-      transform: `translateY(calc(100% - ${theme.spacing(12)}px)) !important`,
-      visibility: 'visible !important',
-
-      '& $bottomSheetContent': {
-        overflow: 'hidden',
-      },
-    },
-  },
-  closed: {},
-  bottomSheetContent: {
-    minHeight: theme.spacing(18),
-    '& > *': {
-      paddingBottom: theme.spacing(6),
-    },
-  },
-  bottomSheetButton: {
-    position: 'absolute',
-    bottom: theme.spacing(5),
-    right: theme.spacing(2),
-    zIndex: theme.zIndex.drawer + 1,
-    color: theme.palette.primary.main,
-    backgroundColor: theme.palette.common.white,
-    transform: `translateY(${theme.spacing(3)}px)`,
-    transition: `transform ${theme.transitions.easing.sharp} ${theme.transitions.duration.shortest}ms`,
-
-    '&:hover': {
-      backgroundColor: theme.palette.common.white,
-    },
-
-    '& .MuiFab-label': {
-      width: theme.spacing(9),
-      justifyContent: 'flex-start',
-    },
-
-    '&$buttonShow': {
-      transform: 'translateY(0)',
-
-      '& $bottomSheetIcon': {
-        transform: 'rotate(0)',
-      },
-    },
-  },
-  bottomSheetIcon: {
-    color: theme.palette.text.hint,
-    height: theme.spacing(4),
-    transform: 'rotate(180deg)',
-  },
-  buttonShow: {},
   mapWrapper: {
     position: 'relative',
     flex: 1,
@@ -143,15 +84,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Main() {
-  const mobileContainer = useRef(null);
-  const desktopContainer = useRef(null);
   const dispatch = useDispatch();
   const error = useSelector((state) => state.app.error);
-  const bottomSheetOpen = useSelector((state) => state.app.bottomSheetOpen);
   const isGmaps = useSelector((state) => BASEMAPS[state.carto.basemap].type === 'gmaps');
-  const theme = useTheme();
   const classes = useStyles();
-  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
 
   // [hygen] Add useEffect
 
@@ -159,63 +95,13 @@ export default function Main() {
     dispatch(setError(null));
   };
 
-  const handleWidgetsDrawerToggle = () => {
-    dispatch(setBottomSheetOpen(!bottomSheetOpen));
-  };
-
   const layers = getLayers();
 
   return (
     <Grid container direction='row' alignItems='stretch' item xs>
       <nav className={classes.drawer}>
-        <Portal container={isMobile ? mobileContainer.current : desktopContainer.current}>
-          <Outlet />
-        </Portal>
-        <Hidden xsDown implementation='css'>
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant='permanent'
-            PaperProps={{
-              elevation: 8,
-            }}
-            open
-          >
-            <Toolbar variant='dense' />
-            <Grid container item xs ref={desktopContainer} />
-          </Drawer>
-        </Hidden>
-        <Hidden smUp implementation='css'>
-          <SwipeableDrawer
-            variant='persistent'
-            anchor='bottom'
-            open={bottomSheetOpen}
-            onOpen={handleWidgetsDrawerToggle}
-            onClose={handleWidgetsDrawerToggle}
-            PaperProps={{
-              className: `${classes.bottomSheet} ${
-                !bottomSheetOpen ? classes.closed : ''
-              }`,
-              elevation: 8,
-            }}
-          >
-            <div ref={mobileContainer} className={classes.bottomSheetContent} />
-          </SwipeableDrawer>
-          <Fab
-            variant='extended'
-            size='small'
-            color='inherit'
-            aria-label={bottomSheetOpen ? 'Hide' : 'Show'}
-            className={`${classes.bottomSheetButton} ${
-              !bottomSheetOpen ? classes.buttonShow : ''
-            }`}
-            onClick={handleWidgetsDrawerToggle}
-          >
-            <ExpandLessIcon className={classes.bottomSheetIcon} />
-            {bottomSheetOpen ? 'Hide' : 'Show'}
-          </Fab>
-        </Hidden>
+        <Desktop />
+        <Mobile />
       </nav>
 
       <Grid item className={`${classes.mapWrapper} ${isGmaps ? classes.gmaps : ''}`}>
@@ -232,5 +118,133 @@ export default function Main() {
         <Alert severity='error'>{error}</Alert>
       </Snackbar>
     </Grid>
+  );
+}
+
+const useStylesDesktopDrawer = makeStyles(() => ({
+  drawerPaper: {
+    width: DRAWER_WIDTH,
+    position: 'absolute',
+  },
+}));
+
+function Desktop() {
+  const classes = useStylesDesktopDrawer();
+
+  return (
+    <Hidden xsDown>
+      <Drawer
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        variant='permanent'
+        anchor='left'
+        open
+      >
+        <Toolbar variant='dense' />
+        <Grid container item xs>
+          <Outlet />
+        </Grid>
+        <Outlet />
+      </Drawer>
+    </Hidden>
+  );
+}
+
+const useStylesMobileDrawer = makeStyles((theme) => ({
+  closed: {},
+  bottomSheet: {
+    maxHeight: `calc(100% - ${theme.spacing(6)}px)`,
+
+    '&$closed': {
+      transform: `translateY(calc(100% - ${theme.spacing(12)}px)) !important`,
+      visibility: 'visible !important',
+
+      '& $bottomSheetContent': {
+        overflow: 'hidden',
+      },
+    },
+  },
+  bottomSheetContent: {
+    minHeight: theme.spacing(18),
+    '& > *': {
+      paddingBottom: theme.spacing(6),
+    },
+  },
+  bottomSheetButton: {
+    position: 'absolute',
+    bottom: theme.spacing(5),
+    right: theme.spacing(2),
+    zIndex: theme.zIndex.drawer + 1,
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.common.white,
+    transform: `translateY(${theme.spacing(3)}px)`,
+    transition: `transform ${theme.transitions.easing.sharp} ${theme.transitions.duration.shortest}ms`,
+
+    '&:hover': {
+      backgroundColor: theme.palette.common.white,
+    },
+
+    '& .MuiFab-label': {
+      width: theme.spacing(9),
+      justifyContent: 'flex-start',
+    },
+
+    '&$buttonShow': {
+      transform: 'translateY(0)',
+
+      '& $bottomSheetIcon': {
+        transform: 'rotate(0)',
+      },
+    },
+  },
+  bottomSheetIcon: {
+    color: theme.palette.text.hint,
+    height: theme.spacing(4),
+    transform: 'rotate(180deg)',
+  },
+  buttonShow: {},
+}));
+
+function Mobile() {
+  const dispatch = useDispatch();
+  const bottomSheetOpen = useSelector((state) => state.app.bottomSheetOpen);
+  const classes = useStylesMobileDrawer();
+
+  const handleWidgetsDrawerToggle = () => {
+    dispatch(setBottomSheetOpen(!bottomSheetOpen));
+  };
+
+  return (
+    <Hidden smUp implementation='css'>
+      <SwipeableDrawer
+        variant='persistent'
+        anchor='bottom'
+        open={bottomSheetOpen}
+        onOpen={handleWidgetsDrawerToggle}
+        onClose={handleWidgetsDrawerToggle}
+        PaperProps={{
+          className: `${classes.bottomSheet} ${!bottomSheetOpen ? classes.closed : ''}`,
+          elevation: 8,
+        }}
+      >
+        <div className={classes.bottomSheetContent}>
+          <Outlet />
+        </div>
+      </SwipeableDrawer>
+      <Fab
+        variant='extended'
+        size='small'
+        color='inherit'
+        aria-label={bottomSheetOpen ? 'Hide' : 'Show'}
+        className={`${classes.bottomSheetButton} ${
+          !bottomSheetOpen ? classes.buttonShow : ''
+        }`}
+        onClick={handleWidgetsDrawerToggle}
+      >
+        <ExpandLessIcon className={classes.bottomSheetIcon} />
+        {bottomSheetOpen ? 'Hide' : 'Show'}
+      </Fab>
+    </Hidden>
   );
 }
