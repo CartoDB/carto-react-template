84205653343d1fbee29caba8b58d0bce264f3b63
Alberto Arana <aaranadev@gmail.com>
2021-06-24 13:55:56 +0200

    added legend and new tool hextorgb

--- template-sample-app/template/src/components/layers/TilesetLayer.js
+++ template-sample-app/template/src/components/layers/TilesetLayer.js
@@ -1,12 +1,40 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { CartoBQTilerLayer, colorBins } from '@deck.gl/carto';
-import { selectSourceById } from '@carto/react-redux';
+import { selectSourceById, updateLayer } from '@carto/react-redux';
 import { useCartoLayerProps } from '@carto/react-api';
 import htmlForFeature from 'utils/htmlForFeature';
+import { LEGEND_TYPES } from '@carto/react-ui';
+import hexToRGB from 'utils/hexToRgb';
 
 export const TILESET_LAYER_ID = 'tilesetLayer';
 
+const COLORS = [
+  '#009392',
+  '#39b185',
+  '#9ccb86',
+  '#e9e29c',
+  '#eeb479',
+  '#e88471',
+  '#cf597e',
+];
+
+const COLORS_RGB = COLORS.map((color) => hexToRGB(color));
+
+const LABELS = [10, 100, 1e3, 1e4, 1e5, 1e6];
+
+const layerConfig = {
+  title: 'State analysis',
+  visible: true,
+  legend: {
+    attr: 'aggregated_total',
+    type: LEGEND_TYPES.BINS,
+    labels: LABELS,
+    colors: COLORS,
+  },
+};
+
 function TilesetLayer() {
+  const dispatch = useDispatch();
   const { tilesetLayer } = useSelector((state) => state.carto.layers);
   const source = useSelector((state) => selectSourceById(state, tilesetLayer?.source));
   const cartoLayerProps = useCartoLayerProps(source);
@@ -19,12 +47,21 @@ function TilesetLayer() {
       pointRadiusUnits: 'pixels',
       lineWidthUnits: 'pixels',
       pickable: true,
+      visible: tilesetLayer.visible,
       getFillColor: colorBins({
-        attr: 'aggregated_total',
-        domain: [10, 100, 1e3, 1e4, 1e5, 1e6],
-        colors: 'Temps',
+        attr: layerConfig.legend.attr,
+        domain: LABELS,
+        colors: COLORS_RGB,
       }),
       pointRadiusMinPixels: 2,
+      onDataLoad: () => {
+        dispatch(
+          updateLayer({
+            id: TILESET_LAYER_ID,
+            layerAttributes: { ...layerConfig },
+          })
+        );
+      },
       onHover: (info) => {
         if (info && info.object) {
           info.object = {
