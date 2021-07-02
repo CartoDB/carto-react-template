f0852b0019a4acba04438e0b605c6adf3cd3ba46
Alberto Arana <aaranadev@gmail.com>
2021-07-02 11:57:48 +0200

    added lazy in maps

--- template-skeleton-cn/template/src/components/common/map/DeckGLComponent.js
+++ template-skeleton-cn/template/src/components/common/map/DeckGLComponent.js
@@ -1,72 +1,22 @@
 import DeckGL from '@deck.gl/react';
-import { useSelector, useDispatch } from 'react-redux';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { useSelector } from 'react-redux';
 import { StaticMap } from 'react-map-gl';
-import { makeStyles, useTheme, useMediaQuery } from '@material-ui/core';
-import { setViewState } from '@carto/react-redux';
+import { useTheme, useMediaQuery } from '@material-ui/core';
 import { BASEMAPS } from '@carto/react-basemaps';
-
-const useStyles = makeStyles((theme) => ({
-  tooltip: {
-    '& .content': {
-      ...theme.typography.caption,
-      position: 'relative',
-      padding: theme.spacing(1, 1.5),
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: theme.palette.grey[900],
-      color: 'rgba(255, 255, 255, 0.75)',
-      transform: `translate(-50%, calc(-100% - ${theme.spacing(2.5)}px))`,
-
-      '& .arrow': {
-        display: 'block',
-        position: 'absolute',
-        top: 'calc(100% - 1px)',
-        left: '50%',
-        width: 0,
-        height: 0,
-        marginLeft: theme.spacing(-1),
-        borderLeft: `${theme.spacing(1)}px solid transparent`,
-        borderRight: `${theme.spacing(1)}px solid transparent`,
-        borderTop: `${theme.spacing(1)}px solid ${theme.palette.grey[900]}`,
-      },
-    },
-  },
-}));
+import { useMapHooks } from './useMapHooks';
 
 export default function DeckGLComponent({ layers }) {
-  const dispatch = useDispatch();
   const viewState = useSelector((state) => state.carto.viewState);
   const basemap = useSelector((state) => BASEMAPS[state.carto.basemap]);
   const theme = useTheme();
-  const classes = useStyles();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
-
-  let isHovering = false;
-
-  const handleViewStateChange = ({ viewState }) => {
-    dispatch(setViewState(viewState));
-  };
-
-  const handleSizeChange = ({ width, height }) => {
-    dispatch(setViewState({ width, height }));
-  };
-
-  const handleHover = ({ object }) => (isHovering = !!object);
-  const handleCursor = ({ isDragging }) =>
-    isDragging ? 'grabbing' : isHovering ? 'pointer' : 'grab';
-
-  const handleTooltip = (info) => {
-    if (info?.object) {
-      return {
-        html: `<div class='content'>${info.object.html}<div class='arrow'></div></div>`,
-        className: classes.tooltip,
-        style: {
-          padding: 0,
-          background: 'none',
-        },
-      };
-    }
-  };
+  const {
+    handleCursor,
+    handleHover,
+    handleSizeChange,
+    handleTooltip,
+    handleViewStateChange,
+  } = useMapHooks();
 
   return (
     <DeckGL
--- template-skeleton-cn/template/src/components/common/map/GoogleMapsComponent.js
+++ template-skeleton-cn/template/src/components/common/map/GoogleMapsComponent.js
@@ -1,63 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { makeStyles } from '@material-ui/core';
-import { setViewState } from '@carto/react-redux';
 import { BASEMAPS, GoogleMap } from '@carto/react-basemaps';
-
-const useStyles = makeStyles((theme) => ({
-  tooltip: {
-    '& .content': {
-      ...theme.typography.caption,
-      position: 'relative',
-      padding: theme.spacing(1, 1.5),
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: theme.palette.grey[900],
-      color: 'rgba(255, 255, 255, 0.75)',
-      transform: `translate(-50%, calc(-100% - ${theme.spacing(2.5)}px))`,
-
-      '& .arrow': {
-        display: 'block',
-        position: 'absolute',
-        top: 'calc(100% - 1px)',
-        left: '50%',
-        width: 0,
-        height: 0,
-        marginLeft: theme.spacing(-1),
-        borderLeft: `${theme.spacing(1)}px solid transparent`,
-        borderRight: `${theme.spacing(1)}px solid transparent`,
-        borderTop: `${theme.spacing(1)}px solid ${theme.palette.grey[900]}`,
-      },
-    },
-  },
-}));
+import { useMapHooks } from './useMapHooks';
 
 export default function GoogleMapsComponent({ layers }) {
-  const dispatch = useDispatch();
   const viewState = useSelector((state) => state.carto.viewState);
   const basemap = useSelector((state) => BASEMAPS[state.carto.basemap]);
   const googleApiKey = useSelector((state) => state.carto.googleApiKey);
-  const classes = useStyles();
-
-  const handleViewStateChange = ({ viewState }) => {
-    dispatch(setViewState(viewState));
-  };
-
-  const handleSizeChange = ({ width, height }) => {
-    dispatch(setViewState({ width, height }));
-  };
-
-  const handleTooltip = (info) => {
-    if (info?.object) {
-      return {
-        html: `<div class='content'>${info.object.html}<div class='arrow'></div></div>`,
-        className: classes.tooltip,
-        style: {
-          padding: 0,
-          background: 'none',
-        },
-      };
-    }
-  };
+  const { handleSizeChange, handleTooltip, handleViewStateChange } = useMapHooks();
 
   return (
     <GoogleMap
