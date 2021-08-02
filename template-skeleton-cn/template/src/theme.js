4fea9c85413ef75ba9116602b0acbd6df1672dfd
Sergio Clebal <sergioclebal@gmail.com>
2021-08-02 13:49:41 +0200

    Rename createMuiTheme to createTheme

--- template-skeleton-cn/template/src/theme.js
+++ template-skeleton-cn/template/src/theme.js
@@ -1,9 +1,9 @@
 import deepmerge from 'deepmerge';
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
 import { cartoThemeOptions } from '@carto/react-ui';
 
 const customTheme = {};
 
-const theme = createMuiTheme(deepmerge(cartoThemeOptions, customTheme));
+const theme = createTheme(deepmerge(cartoThemeOptions, customTheme));
 
 export default theme;
