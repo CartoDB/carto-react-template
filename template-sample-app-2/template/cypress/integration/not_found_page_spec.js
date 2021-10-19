c6de1c5805e2799beb747bedd2a31b3c346ec565
Víctor Velarde <victor.velarde@gmail.com>
2021-10-19 12:30:14 +0200

    Adapt e2e test to simplified 404 management

--- template-sample-app-2/template/cypress/integration/not_found_page_spec.js
+++ template-sample-app-2/template/cypress/integration/not_found_page_spec.js
@@ -4,10 +4,12 @@ const HOME_PAGE = config.ROOT_PAGE;
 
 describe('Not Found Page', () => {
   it('redirect to notFound page works', () => {
-    cy.visit(`${HOME_PAGE}/anInvalidPathInTheApp`);
+    const INVALID_PATH = 'anInvalidPathInTheApp';
+    cy.visit(`${HOME_PAGE}/${INVALID_PATH}`);
 
     cy.location().should((loc) => {
-      expect(loc.pathname).to.eq(`/${config.NOT_FOUND_PATH}`);
+      expect(loc.pathname).to.eq(`/${INVALID_PATH}`);
     });
+    cy.findByText('Page not found');
   });
 });
