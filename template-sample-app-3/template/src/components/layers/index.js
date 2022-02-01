fe3067a0566ff4792aa86fd74b878c498349b1ea
Víctor Velarde <victor.velarde@gmail.com>
2022-02-01 20:36:56 +0100

    Adapt template-sample-app-3 to use renamed lasso tool items

--- template-sample-app-3/template/src/components/layers/index.js
+++ template-sample-app-3/template/src/components/layers/index.js
@@ -1,13 +1,13 @@
 import StoresLayer from './StoresLayer';
 import TilesetLayer from './TilesetLayer';
-import { DrawingToolLayer } from '@carto/react-widgets';
+import { FeatureSelectionLayer } from '@carto/react-widgets';
 // [hygen] Import layers
 
 export const getLayers = () => {
   return [
     StoresLayer(),
     TilesetLayer(),
-    DrawingToolLayer(),
+    FeatureSelectionLayer(),
     // [hygen] Add layer
   ];
 };
