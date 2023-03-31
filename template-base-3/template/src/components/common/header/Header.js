8467469ef469bb498f9a7fd62782ea6c64f05bcf
Verónica Milán <vmilan@cartodb.com>
2023-03-31 17:19:38 +0200

    header refactor

---
+++ template-base-3/template/src/components/common/header/Header.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Divider, Hidden, Grid } from '@mui/material';
+import { AppBar } from '@carto/react-ui';
+import DrawerMenu from './DrawerMenu';
+import NavigationMenu from './NavigationMenu';
+import Logo from './Logo';
+import UserMenu from './UserMenu';
+import { styled } from '@mui/material/styles';
+
+export const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  zIndex: theme.zIndex.modal + 1,
+}));
+
+export const StyledDivider = styled(Divider)(({ theme }) => ({
+  marginLeft: theme.spacing(1),
+}));
+
+export default function Header() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    setDrawerOpen(false);
+  }, []);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <StyledAppBar
+      component='header'
+      position='relative'
+      showBurgerMenu
+      onClickMenu={handleDrawerToggle}
+      brandLogo={<Logo />}
+      brandText='React Demo'
+    >
+      <Hidden mdUp>
+        <DrawerMenu drawerOpen={drawerOpen} handleDrawerToggle={handleDrawerToggle} />
+      </Hidden>
+
+      <Hidden mdDown>
+        <StyledDivider orientation='vertical' flexItem light></StyledDivider>
+
+        <NavigationMenu />
+        <Grid container item xs justifyContent='flex-end'>
+          <UserMenu />
+        </Grid>
+      </Hidden>
+    </StyledAppBar>
+  );
+}
---
+++ template-base-3/template/src/components/common/header/NavigationMenu.js
@@ -0,0 +1,54 @@
+import { Grid, Tab, Tabs } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import { NavLink, useLocation } from 'react-router-dom';
+import { ROUTE_PATHS } from 'routes';
+
+const useStylesNavigationMenu = makeStyles((theme) => ({
+  navTabs: {
+    '& .MuiTab-root': {
+      color: theme.palette.common.white,
+
+      '&:hover': {
+        borderBottomColor: theme.palette.common.white,
+      },
+    },
+    '& .MuiTabs-indicator': {
+      backgroundColor:
+        theme.palette.appBar?.contrastText || theme.palette.primary?.contrastText,
+    },
+  },
+  verticalNavTabs: {
+    '& .MuiTabs-root': {
+      boxShadow: 'none',
+    },
+
+    '& .MuiTab-root': {
+      justifyContent: 'flex-start',
+      paddingLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function NavigationMenu({ column: vertical }) {
+  const location = useLocation();
+  const classes = useStylesNavigationMenu();
+
+  const pathname = location.pathname.split('/')[1] || false;
+
+  return (
+    <Grid
+      container
+      direction={vertical ? 'column' : 'row'}
+      className={!vertical ? classes.navTabs : classes.verticalNavTabs}
+    >
+      <Tabs
+        value={pathname}
+        textColor={vertical ? 'primary' : 'inherit'}
+        orientation={vertical ? 'vertical' : 'horizontal'}
+      >
+        <Tab label='Home' value='' component={NavLink} to={ROUTE_PATHS.DEFAULT} />
+        {/* [hygen] Import links */}
+      </Tabs>
+    </Grid>
+  );
+}
