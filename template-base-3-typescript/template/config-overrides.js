924fe013eda3623f706246170e9de85e70c7f945
Verónica Milán <vmilan@cartodb.com>
2023-03-31 17:47:30 +0200

    bump mui and c4r versions

--- template-base-3-typescript/template/config-overrides.js
+++ template-base-3-typescript/template/config-overrides.js
@@ -8,7 +8,7 @@ module.exports = function override(config, env) {
     ...newConfig.resolve.alias,
     react: path.resolve('./node_modules/react'),
     'react-redux': path.resolve('./node_modules/react-redux'),
-    '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
+    '@mui/material': path.resolve('./node_modules/@mui/material'),
     '@deck.gl/core': path.resolve('./node_modules/@deck.gl/core'),
     '@deck.gl/google-maps': path.resolve('./node_modules/@deck.gl/google-maps'),
     '@deck.gl/extensions': path.resolve('./node_modules/@deck.gl/extensions'),
