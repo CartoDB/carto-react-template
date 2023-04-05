d31de25af76a9715af919f9d070ea4c88b95b229
Víctor Velarde <victor.velarde@gmail.com>
2023-04-05 20:49:38 +0200

    Remove carto-2 templates

--- template-base-2/template/src/components/layers/index.js
+++
@@ -1,7 +0,0 @@
-// [hygen] Import layers
-
-export const getLayers = () => {
-  return [
-    // [hygen] Add layer
-  ];
-};
--- template-sample-app-2/template/src/components/common/LazyLoadRoute.js
+++
@@ -1,6 +0,0 @@
-import { Suspense } from 'react';
-import TopLoading from './TopLoading';
-
-export default function LazyLoadRoute({ children, fallback = <TopLoading /> }) {
-  return <Suspense fallback={fallback}>{children}</Suspense>;
-}
