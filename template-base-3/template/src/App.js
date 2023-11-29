b0fb1a77516a99b531874343954c8ddb09393f69
Víctor Velarde <victor.velarde@gmail.com>
2023-11-29 20:07:11 +0100

    Upgrade to version 2.3

--- template-base-3/template/src/App.js
+++ template-base-3/template/src/App.js
@@ -5,6 +5,7 @@ import LazyLoadRoute from 'components/common/LazyLoadRoute';
 import theme from './theme';
 import routes from './routes';
 import useAuth from './hooks/Auth0';
+import { IntlProvider } from 'react-intl';
 
 const GridApp = styled(Grid)(() => ({
   flex: '1 1 auto',
@@ -17,13 +18,15 @@ export default function App() {
   useAuth();
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <GridApp container>
-          <LazyLoadRoute>{routing}</LazyLoadRoute>
-        </GridApp>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <IntlProvider locale='en'>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <GridApp container>
+            <LazyLoadRoute>{routing}</LazyLoadRoute>
+          </GridApp>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </IntlProvider>
   );
 }
